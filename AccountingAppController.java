7f210b1a61df4a905d74609c703a2cbe56a9af1b
IonutCornea12 <117024490+IonutCornea12@users.noreply.github.com>
2023-12-16 17:29:53 +0200

    update accountingappcontroller for the transactiondetails tab

--- AccountingAppController.java
+++ AccountingAppController.java
@@ -1,4 +1,4 @@
-package com.example.proiect2;
+package com.example.BalanceBuddy;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -24,6 +24,8 @@ public class AccountingAppController implements Initializable {
     @FXML
     private TableView<Transaction> transactionTable;
     @FXML
+    private TableView<TransactionDetails> transactionDetailsTable;
+    @FXML
     private TableColumn<Transaction, Date> dateColumn;
     @FXML
     private TableColumn<Transaction, Integer> idColumn;
@@ -35,6 +37,19 @@ public class AccountingAppController implements Initializable {
     private TableColumn<Transaction, String> measureUnitColumn;
 
     @FXML
+    private TableColumn<TransactionDetails, Integer> detailsTransactionIdColumn;
+    @FXML
+    private TableColumn<TransactionDetails, Double> detailsPriceColumn;
+    @FXML
+    private TableColumn<TransactionDetails, Integer> detailsTvaColumn;
+    @FXML
+    private TableColumn<TransactionDetails, Double> detailsPriceNoTvaColumn;
+    @FXML
+    private TableColumn<TransactionDetails, Integer> detailsDiscountColumn;
+    @FXML
+    private TableColumn<TransactionDetails, Double> detailsTotalColumn;
+
+    @FXML
     private TextField dateTextField;
     @FXML
     private TextField descriptionTextField;
@@ -42,15 +57,39 @@ public class AccountingAppController implements Initializable {
     private TextField amountTextField;
     @FXML
     private TextField measureUnitTextField;
+
+
+    @FXML
+    private TextField priceTextField;
+    @FXML
+    private TextField tvaTextField;
+    @FXML
+    private TextField discountTextField;
+    @FXML
+    private TextField transactionIdTextField;
+
+
     @FXML
     private Button addTransactionButton;
     @FXML
     private Button deleteTransactionButton;
+
+    @FXML
+    private Button addDetailsButton;
+    @FXML
+    private Button deleteDetailsButton;
+
+    @FXML
+    private Button refreshDetailsButton;
+
     @FXML
     private Label userEmailLabel;
     @FXML
     private Button refreshButton;
     private String userEmail;
+
+
+
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.idColumn.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
@@ -59,17 +98,29 @@ public class AccountingAppController implements Initializable {
         this.descriptionColumn.setCellValueFactory(cellData -> cellData.getValue().descriptionProperty());
         this.measureUnitColumn.setCellValueFactory(cellData -> cellData.getValue().measureUnitProperty());
 
-        this.addTransactionButton.setOnAction(event -> this.addTransaction());
-        this.userEmailLabel.setText("Welcome to the accounting app: " + this.userEmail);
+        this.detailsTransactionIdColumn.setCellValueFactory(cellData -> cellData.getValue().transactionIdProperty().asObject());
+        this.detailsPriceColumn.setCellValueFactory(cellData -> cellData.getValue().priceProperty().asObject());
+        this.detailsTvaColumn.setCellValueFactory(cellData -> cellData.getValue().tvaProperty().asObject());
+        this.detailsPriceNoTvaColumn.setCellValueFactory(cellData -> cellData.getValue().priceNoTvaProperty().asObject());
+        this.detailsDiscountColumn.setCellValueFactory(cellData -> cellData.getValue().discountProperty().asObject());
+        this.detailsTotalColumn.setCellValueFactory(cellData -> cellData.getValue().totalProperty().asObject());
+
+
+        this.userEmailLabel.setText("Welcome to BallanceBuddy: " + this.userEmail);
         this.loadDataFromDatabase();
+        this.loadDataDetailsFromDatabase();
+        this.addTransactionButton.setOnAction(event -> this.addTransaction());
         this.deleteTransactionButton.setOnAction(event -> this.deleteTransaction());
         this.refreshButton.setOnAction(event -> this.refreshDataFromDatabase());
+        this.addDetailsButton.setOnAction(event -> this.addTransactionDetails());
+        this.deleteDetailsButton.setOnAction(event -> this.deleteTransactionDetails());
+        this.refreshDetailsButton.setOnAction(event -> this.refreshDataDetailsFromDatabase());
+
     }
 
     public void setUserEmail(String userEmail) {
         this.userEmail = userEmail;
     }
-    @FXML
     private void deleteTransaction() {
         Transaction selectedTransaction = transactionTable.getSelectionModel().getSelectedItem();
         if (selectedTransaction != null) {
@@ -163,7 +214,6 @@ public class AccountingAppController implements Initializable {
             logger.log(Level.SEVERE, "Error in database operation", var17);
         }
     }
-
     private void addTransaction() {
         try {
             int id = 0;
@@ -193,5 +243,130 @@ public class AccountingAppController implements Initializable {
             logger.log(Level.SEVERE, "Error in adding transaction", var8);
         }
     }
+    private void addTransactionDetails() {
+        try {
 
+            int transactionId = this.transactionIdTextField.getText().isEmpty() ? (int) 1.0 : Integer.parseInt(this.transactionIdTextField.getText());
+            double price = this.priceTextField.getText().isEmpty() ? 1.0 : Double.parseDouble(this.priceTextField.getText());
+            int tva = this.tvaTextField.getText().isEmpty() ? (int) 1.0 : Integer.parseInt(this.tvaTextField.getText());; // Default value
+            int discount = this.discountTextField.getText().isEmpty() ? (int) 1.0 : Integer.parseInt(this.discountTextField.getText());
+            double priceNoTva = price - (tva / 100.0) * price; // Calculate priceNoTva
+            double total = price - (discount / 100.0) * price; // Calculate total
+            TransactionDetails newDetails = new TransactionDetails(transactionId, price, tva, priceNoTva, discount, total);
+
+            Platform.runLater(() ->
+                    this.transactionDetailsTable.getItems().add(newDetails)
+            );
+
+            boolean success = DbFunctions.addTransactionDetails(newDetails);
+            if (success) {
+                System.out.println("Transaction added successfully");
+            } else {
+                System.err.println("Failed to add transaction to the database");
+            }
+            this.transactionIdTextField.clear();
+            this.priceTextField.clear();
+            this.tvaTextField.clear();
+            this.discountTextField.clear();
+        } catch (IllegalArgumentException var8) {
+            Logger logger = Logger.getLogger(AccountingAppController.class.getName());
+            logger.log(Level.SEVERE, "Error in adding transaction", var8);
+        }
+    }
+    private void deleteTransactionDetails() {
+        TransactionDetails selectedTransactionDet = transactionDetailsTable.getSelectionModel().getSelectedItem();
+        if (selectedTransactionDet != null) {
+            boolean success = DbFunctions.deleteTransactionDetails(selectedTransactionDet.getTransactionId());
+            if (success) {
+                transactionDetailsTable.getItems().remove(selectedTransactionDet);
+                System.out.println("Transaction deleted successfully");
+            } else {
+                System.err.println("Failed to delete transaction from the database");
+            }
+        } else {
+            System.err.println("No transaction selected for deletion");
+        }
+    }
+
+    private void loadDataDetailsFromDatabase() {
+        String query = "SELECT * FROM transaction_details";
+
+        try {
+            Connection con = DbFunctions.connect();
+
+            try {
+                PreparedStatement pst = con.prepareStatement(query);
+
+                try {
+                    ResultSet resultSet = pst.executeQuery();
+
+                    try {
+                        while (resultSet.next()) {
+                            int transactionId = resultSet.getInt("transactionid");
+                            double price = resultSet.getDouble("price");
+                            int tva = resultSet.getInt("tva");
+                            int discount = resultSet.getInt("discount");
+                            double priceNoTva = resultSet.getDouble("price_no_tva");
+                            double total = resultSet.getDouble("total");
+
+                            TransactionDetails transactionDetails = new TransactionDetails(transactionId, price, tva, priceNoTva, discount, total);
+
+                            Platform.runLater(() ->
+                                    this.transactionDetailsTable.getItems().add(transactionDetails)
+                            );
+                        }
+                    } catch (Throwable var14) {
+                        if (resultSet != null) {
+                            try {
+                                resultSet.close();
+                            } catch (Throwable var13) {
+                                var14.addSuppressed(var13);
+                            }
+                        }
+                        throw var14;
+                    } finally {
+                        if (resultSet != null) {
+                            resultSet.close();
+                        }
+                    }
+                } catch (Throwable var15) {
+                    if (pst != null) {
+                        try {
+                            pst.close();
+                        } catch (Throwable var12) {
+                            var15.addSuppressed(var12);
+                        }
+                    }
+                    throw var15;
+                } finally {
+                    if (pst != null) {
+                        pst.close();
+                    }
+                }
+            } catch (Throwable var16) {
+                if (con != null) {
+                    try {
+                        con.close();
+                    } catch (Throwable var11) {
+                        var16.addSuppressed(var11);
+                    }
+                }
+                throw var16;
+            } finally {
+                if (con != null) {
+                    con.close();
+                }
+            }
+        } catch (SQLException var17) {
+            Logger logger = Logger.getLogger(AccountingAppController.class.getName());
+            logger.log(Level.SEVERE, "Error in database operation", var17);
+        }
+    }
+    private void refreshDataDetailsFromDatabase() {
+        // Clear existing data
+        this.transactionDetailsTable.getItems().clear();
+
+        // Reload data from the database
+        loadDataDetailsFromDatabase();
+    }
 }
